00c709354dc19508e5cce863a21344a159d0bcd9
Imran11150 <imranmehemmedeli82@gmail.com>
2025-03-03 20:55:55 +0400

    app fille html css js

---
+++ app.js
@@ -0,0 +1,18 @@
+let numb01 = 0;
+let numb02 = 0;
+let result = 0;
+
+
+function sum(){
+numb01 = document.getElementById("first-num").volue
+numb02 = document.getElementById("second-num").volue
+result = numb01+numb02
+document.getElementById("result").innerHTML = result;
+}
+
+
+
+
+
+
+
